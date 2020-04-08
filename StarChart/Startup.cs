266da17f40bc03ede3e657f0229b0f9b25bc833a
MATHEW <vipinmathew2013@gmail.com>
2020-04-08 09:29:02 +0530

    initial commit

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -13,7 +13,7 @@ namespace StarChart
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
-            services.AddDbContext<ApplicationDbContext> (options => options.UseInMemoryDatabase("StarChart");
+            services.AddDbContext<ApplicationDbContext> (options => options.UseInMemoryDatabase("StarChart"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
