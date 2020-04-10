850ce7c3c66d5b040b643417be7d1a37dce05254
MATHEW <vipinmathew2013@gmail.com>
2020-04-10 19:50:09 +0530

    test

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -11,11 +11,11 @@ namespace StarChart.Controllers
     [ApiController]
     public class CelestialObjectController : ControllerBase
     {
-        public readonly ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
 
         public CelestialObjectController(ApplicationDbContext context)
         {
-            _context = context;
+            context = _context;
         }
     }
 }
