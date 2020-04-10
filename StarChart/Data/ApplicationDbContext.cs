2a23501bac3dfa4d56b0ffdcf28e90ba17e77a80
MATHEW <vipinmathew2013@gmail.com>
2020-04-10 19:04:22 +0530

    test

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -7,9 +7,9 @@ namespace StarChart.Data
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
+            public DbSet<CelestialObject> CelestialObjects;
 
-
-        }
-        public DbSet<CelestialObject> CelestialObjects;
+    }
+        
     }
 }
